96dee6c81423aa877cf4ca543927e454dbe66e7b
JustinFrost47 <rinojonathanarul@gmail.com>
2023-10-07 11:15:36 +0530

    changed offline behaviour

--- sw.js
+++ sw.js
@@ -48,39 +48,39 @@ self.addEventListener("activate", (event) => {
 // On fetch, intercept server requests
 // and respond with cached responses instead of going to network
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate" || event.request.method === "GET") {
+  if (event.request.mode === "navigate") {
     event.respondWith(
+      // Try to serve the cached HTML page for navigation requests.
       caches.match(event.request).then((response) => {
         return response || fetch(event.request).then((fetchResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
+          // Cache the fetched response for future use.
+          caches.open(CACHE_NAME).then((cache) => {
             cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
           });
-        });
-      }).catch((error) => {
-        console.error("Fetch error:", error);
-        return new Response("Sorry, something went wrong. Please try again later.", {
-          headers: { "Content-Type": "text/plain" },
+          return fetchResponse;
+        }).catch(() => {
+          // If fetching from network fails, return the cached HTML page.
+          return caches.match("/");
         });
       })
     );
     return;
   }
 
-
-
-
-  // For all other requests, go to the cache first, and then the network.
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        // Return the cached response if it's available.
-        return cachedResponse;
-      }
-      // If resource isn't in the cache, return a 404.
-      return new Response(null, { status: 404 });
-    })()
-  );
+  if (event.request.method === "GET") {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request).then((fetchResponse) => {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+          });
+          return fetchResponse;
+        });
+      }).catch(() => {
+        // For other requests (e.g., images, scripts), serve from cache if available.
+        return new Response(null, { status: 404 });
+      })
+    );
+    return;
+  }
 });
